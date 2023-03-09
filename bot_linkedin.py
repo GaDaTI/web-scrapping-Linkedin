472bde75be29f9ddbf441e1640adf0010215caf6
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-03-09 11:54:16 -0300

    Acessando a barra de pesquisa

--- bot_linkedin.py
+++ bot_linkedin.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.By import By 
+from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
 import time
 
@@ -7,16 +7,28 @@ import time
 browser = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
 
 # Acessando a pagina do Linkedin
-browser.get("https://www.login.com/login")
+browser.get("https://www.linkedin.com/login")
 
 # Acessando login
-inpt_login = browser.find_element(By.XPATH,"")
+time.sleep(3)
+inpt_login = browser.find_element(By.XPATH,"//*[@id='username']")
 inpt_login.send_keys("prof.gabrielsantana@gmail.com")
 
 # Acessando senha
-inpt_senha = browser.find_element(By.XPATH, "")
-inpt_login.send_keys("")
+time.sleep(3)
+inpt_senha = browser.find_element(By.XPATH, "//*[@id='password']")
+inpt_senha.send_keys("G@Da!978")
 
 # Acessando botão de login
-btn_acesso = browser.find_element(By.XPATH, "")
+time.sleep(5)
+btn_acesso = browser.find_element(By.XPATH, "//*[@id='organic-div']/form/div[3]/button")
 btn_acesso.click()
+
+# Acessando a barra de pesquisa
+time.sleep(3)
+inpt_pesquisa = browser.find_element(By.XPATH, "//*[@id='global-nav-typeahead']/input")
+inpt_pesquisa.send_keys("Desenvolvedor Sênior")
+time.sleep(2)
+inpt_pesquisa.send_keys(Keys.RETURN)
+
+input("")
